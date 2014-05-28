0a26ffe41b73329409375f5698ecfcb91e30dfd2
rojascle <edward-alexander.rojas-clavijo@ensimag.grenoble-inp.fr>
2014-05-28 14:55:52 +0200

    Structure de base

---
+++ crystal-crush/public_html/js/crystal-crush.js
@@ -0,0 +1,15 @@
+// Example by https://twitter.com/awapblog
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+
+function preload() {
+    
+}
+
+function create() {
+    
+}
+
+function update() {
+    
+}
