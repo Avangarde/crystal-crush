129f727e831457a310b483a4d45a5f5fd5b9a19f
Juan Manuel Martinez Romero <juan-manuel.martinez-romero@ensimag.grenoble-inp.fr>
2014-05-28 16:05:17 +0200

    Full Screen Canvas

--- crystal-crush/public_html/js/crystal-crush.js
+++ crystal-crush/public_html/js/crystal-crush.js
@@ -1,15 +1,20 @@
 // Example by https://twitter.com/awapblog
+// Modified by Avantgarde
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+var canvasWidth=window.innerWidth;
+var canvasHeight=window.innerHeight;
+
+var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, '', {preload: preload, create: create});
 
 function preload() {
-    
+    game.load.image('logo', 'phaser.png');
 }
 
 function create() {
-    
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
 }
 
 function update() {
-    
+
 }
