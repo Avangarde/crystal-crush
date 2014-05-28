4f55d10f72bcbe3ad99ef0f6f00e81e90ff52e70
rojascle <edward-alexander.rojas-clavijo@ensimag.grenoble-inp.fr>
2014-05-28 16:13:30 +0200

    Remplisage de grille (premier partie)

--- crystal-crush/public_html/js/crystal-crush.js
+++ crystal-crush/public_html/js/crystal-crush.js
@@ -1,15 +1,38 @@
 // Example by https://twitter.com/awapblog
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'crystal-crush', { preload: preload, create: create, update: update });
+
+var BOARD_COLS = 6;
+var BOARD_ROWS = 6;
+var ELEM_SIZE;
+var ELEM_SPACING;
+var ELEM_SIZE_SPACED = ELEM_SIZE + ELEM_SPACING;
+
+var CU = "CU";
+var ZN = "ZN";
+var NA = "NA";
+var CL = "CL";
+
+var elements;
+
 
 function preload() {
     
 }
 
 function create() {
-    
+    fillBoard();
 }
 
 function update() {
     
+}
+
+function fillBoard() {
+    elements = game.add.group();
+    for (var i = 0; i < BOARD_COLS; i++) {
+        for (var j = 0; j < BOARD_ROWS; j++) {
+            //Create random element
+        }
+    }
 }
