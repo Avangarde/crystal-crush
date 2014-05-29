4e3368aa2c6ba83060a91d74446a5f230f3824b4
Juan Manuel Martinez Romero <juan-manuel.martinez-romero@ensimag.grenoble-inp.fr>
2014-05-29 10:48:28 +0200

    Canvas filled with elements

--- crystal-crush/public_html/js/crystal-crush.js
+++ crystal-crush/public_html/js/crystal-crush.js
@@ -1,35 +1,21 @@
 // Example by https://twitter.com/awapblog
 // Modified by Avantgarde
 
-var canvasWidth = window.innerWidth;
-var canvasHeight = window.innerHeight;
-var scorePanelWidth = canvasWidth * 0.3;
-var gamePanelWidth = canvasWidth * 0.7;
 
 var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, '', {preload: preload, create: create});
-var BOARD_COLS = 6;
-var BOARD_ROWS = 6;
-var ELEM_SIZE;
-var ELEM_SPACING;
-var ELEM_SIZE_SPACED = ELEM_SIZE + ELEM_SPACING;
-
-var CU = "CU";
-var ZN = "ZN";
-var NA = "NA";
-var CL = "CL";
-var A = "A";
-var B = "B";
-
-var elements;
 
 function preload() {
-    game.load.image('logo', 'phaser.png');
+    game.load.image(CU, 'assets/sprites/Cu.png');
+    game.load.image(ZN, 'assets/sprites/Zn.png');
+    game.load.image(NA, 'assets/sprites/Na.png');
+    game.load.image(CL, 'assets/sprites/Cl.png');
+    game.load.image(A, 'assets/sprites/A.png');
+    game.load.image(B, 'assets/sprites/B.png');
+
 }
 
 function create() {
     fillBoard();
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
 }
 
 function update() {
@@ -38,9 +24,27 @@ function update() {
 
 function fillBoard() {
     elements = game.add.group();
+    var boardRowsAndColumns = canvasWidth > canvasHeight ?
+            canvasHeight / BOARD_ROWS :
+            canvasWidth / BOARD_COLS;
     for (var i = 0; i < BOARD_COLS; i++) {
         for (var j = 0; j < BOARD_ROWS; j++) {
-            //Create random element
+            //TODO Cambiar por un elemento aleatorio
+            var element = elements.create(i * ELEM_SIZE_SPACED, j * ELEM_SIZE_SPACED, CU);
+            element.width = boardRowsAndColumns;
+            element.height = boardRowsAndColumns;
+            setElementPosition(element, i, j);
+            selectedGemStartPos = {x: 0, y: 0};
         }
     }
+}
+
+function setElementPosition(elem, posX, posY) {
+    elem.posX = posX;
+    elem.posY = posY;
+    elem.id = calcGemId(posX, posY);
+}
+
+function calcGemId(posX, posY) {
+    return posX + posY * BOARD_COLS;
 }
---
+++ crystal-crush/public_html/js/variables.js
@@ -0,0 +1,24 @@
+var canvasWidth = window.innerWidth;
+var canvasHeight = window.innerHeight;
+var scorePanelWidth = canvasWidth * 0.3;
+var gamePanelWidth = canvasWidth * 0.7;
+
+var BOARD_COLS = 6;
+var BOARD_ROWS = 6;
+//var ELEM_SIZE=64;
+//var ELEM_SPACING=90;
+//var ELEM_SIZE_SPACED = ELEM_SIZE + ELEM_SPACING;
+ELEM_SIZE_SPACED = canvasHeight/BOARD_COLS;
+
+var CU = "CU";
+var ZN = "ZN";
+var NA = "NA";
+var CL = "CL";
+var A = "A";
+var B = "B";
+
+var elements;
+var selectedElementStartPos;
+
+
+
