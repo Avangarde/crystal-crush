29c7130b09a9712bb925e94d1fde2062933786c3
rojascle <edward-alexander.rojas-clavijo@ensimag.grenoble-inp.fr>
2014-05-29 16:08:10 +0200

    Distribution des panneaux du score et du jeux dans l'ecran

--- crystal-crush/public_html/js/variables.js
+++ crystal-crush/public_html/js/variables.js
@@ -1,14 +1,17 @@
 var canvasWidth = window.innerWidth;
-var canvasHeight = window.innerHeight;
-var scorePanelWidth = canvasWidth * 0.3;
-var gamePanelWidth = canvasWidth * 0.7;
+var canvasHeight = canvasWidth * 10/16;
+var margin = canvasHeight * 0.015;
+var scorePanelWidth = (canvasWidth * 0.3) - (2*margin);
+var scorePanelHeight = canvasHeight - (2*margin);
+var gamePanelWidth = canvasHeight - (2*margin);
+var gamePanelHeight = canvasHeight - (2*margin);
 
 var BOARD_COLS = 6;
 var BOARD_ROWS = 6;
 //var ELEM_SIZE=64;
 //var ELEM_SPACING=90;
 //var ELEM_SIZE_SPACED = ELEM_SIZE + ELEM_SPACING;
-ELEM_SIZE_SPACED = canvasHeight/BOARD_COLS;
+ELEM_SIZE_SPACED = (gamePanelHeight-(2*margin))/BOARD_COLS;
 
 var CU = "CU";
 var ZN = "ZN";
