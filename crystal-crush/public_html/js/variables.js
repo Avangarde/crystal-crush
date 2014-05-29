1c495ecd12f0a26dcd816eb914d7ce4320a4645f
Sergio <>
2014-05-29 18:05:52 +0200

    Merge?

--- crystal-crush/public_html/js/variables.js
+++ crystal-crush/public_html/js/variables.js
@@ -1,14 +1,15 @@
 var canvasWidth = window.innerWidth;
-var canvasHeight = window.innerHeight;
-var scorePanelWidth = canvasWidth * 0.3;
-var gamePanelWidth = canvasWidth * 0.7;
+var canvasHeight = canvasWidth * 10 / 16;
+var margin = canvasHeight * 0.015;
+var scorePanelWidth = (canvasWidth * 0.3) - (2 * margin);
+var scorePanelHeight = canvasHeight - (2 * margin);
+var gamePanelWidth = canvasHeight - (2 * margin);
+var gamePanelHeight = canvasHeight - (2 * margin);
 
 var BOARD_COLS = 6;
 var BOARD_ROWS = 6;
-//var ELEM_SIZE=64;
-//var ELEM_SPACING=90;
-//var ELEM_SIZE_SPACED = ELEM_SIZE + ELEM_SPACING;
-ELEM_SIZE_SPACED = canvasHeight/BOARD_COLS;
+var MATCH_MIN = 3;
+var ELEM_SIZE = (gamePanelHeight - (2 * margin)) / BOARD_COLS;
 
 var CU = "CU";
 var ZN = "ZN";
@@ -16,10 +17,14 @@ var NA = "NA";
 var CL = "CL";
 var A = "A";
 var B = "B";
-var elemNames = [CU,ZN,NA,CL,A,B];
+var elemNames = [CU, ZN, NA, CL, A, B];
 
+var allowInput;
 var elements;
+var selectedElement;
 var selectedElementStartPos;
+var selectedElemTween;
+var tempShiftedElem = null;
 
 
 
