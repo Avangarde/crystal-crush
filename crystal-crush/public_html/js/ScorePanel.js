6056e7b224d7add0e0b78f01e96ca11ca40c6df7
Guido SOFER INGLESI <sofering@ensimag.fr>
2014-06-02 16:44:32 +0200

    scorePanel cuenta los elementos acumulados parse

--- crystal-crush/public_html/js/ScorePanel.js
+++ crystal-crush/public_html/js/ScorePanel.js
@@ -7,24 +7,32 @@ ScorePanel = function(game, x ,y, width, heigth) {
     this.width = width;
     this.heigth = heigth;
     this.background;
-    this.abc0;
-    this.abc1;
-    this.abc2;
-    this.abc3;
-    this.abc4;
-    this.abc5;
-    this.abc6;
 
+    this.score_general = 0;
+    this.cu_count = 0;
+    this.zn_count = 0;
+    this.na_count = 0;
+    this.cl_count = 0;
+    this.a_count  = 0;
+    this.b_count  = 0;
+
+    this.score_txt;
+    this.cu_txt;
+    this.zn_txt;
+    this.na_txt;
+    this.cl_txt;
+    this.a_txt;
+    this.b_txt;
 };
 
 ScorePanel.prototype = {
 preload: function() {
-             game.load.image('scorePanelBackgroung', 'assets/scorePanel.png');
+             game.load.image('scorePanelBackground', 'assets/scorePanel.png');
              game.load.spritesheet('createButton', 'assets/buttons/button_sprite_sheet.png', 193, 71);
          },
 
 create: function() {
-            this.background = game.add.sprite(this.x, this.y, 'scorePanelBackgroung');
+            this.background = game.add.sprite(this.x, this.y, 'scorePanelBackground');
             this.background.width = this.width;
             this.background.height = this.height;
 
@@ -33,26 +41,51 @@ create: function() {
 
 
             var decal = 17;
-            this.abc0 = game.add.text(10,10,'Score: '+score_general,style1);
-            game.add.text(10,10+1*decal,count_cu_text,style2);
-            game.add.text(10,10+2*decal,count_zn_text,style2);
-            game.add.text(10,10+3*decal,count_na_text,style2);
-            game.add.text(10,10+4*decal,count_cl_text,style2);
-            game.add.text(10,10+5*decal,count_a_text,style2);
-            game.add.text(10,10+6*decal,count_b_text,style2);
+            this.score_txt = game.add.text(10,10,         'Score : ' + this.score_general , style1);
+            this.cu_txt    = game.add.text(10,10+1*decal, 'Cu : '    + this.cu_count , style2);
+            this.zn_txt    = game.add.text(10,10+2*decal, 'Zn : '    + this.zn_count , style2);
+            this.na_txt    = game.add.text(10,10+3*decal, 'Na : '    + this.na_count , style2);
+            this.cl_txt    = game.add.text(10,10+4*decal, 'Cl : '    + this.cl_count , style2);
+            this.a_txt     = game.add.text(10,10+5*decal, 'A : '     + this.a_count  , style2);
+            this.b_txt     = game.add.text(10,10+6*decal, 'B : '     + this.b_count  , style2);
         },
 
 update: function() {
-        this.abc0.text = 'Score: ' + score_general;
+        this.score_txt.text = 'Score : ' + this.score_general;
+        this.cu_txt.text    = 'Cu : '    + this.cu_count;
+        this.zn_txt.text    = 'Zn : '    + this.zn_count;
+        this.na_txt.text    = 'Na : '    + this.na_count; 
+        this.cl_txt.text    = 'Cl : '    + this.cl_count; 
+        this.a_txt.text     = 'A : '     + this.a_count ; 
+        this.b_txt.text     = 'B : '     + this.b_count ; 
         },
+
+addMatch2: function(elem_name, count) {
+        if(elem_name=='CU'){
+            this.cu_count = this.cu_count + count;
+        }else if(elem_name=='ZN'){
+            this.zn_count = this.zn_count + count;
+        }else if(elem_name=='NA'){
+            this.na_count = this.na_count + count;
+        }else if(elem_name=='CL'){
+            this.cl_count = this.cl_count + count;
+        }else if(elem_name=='A'){
+            this.a_count = this.a_count + count;
+        }else if(elem_name=='B'){
+            this.b_count = this.b_count + count;
+        }
+    },
 
 addMatch: function(countHoriz, countVert, elem_name) {
     if(countHoriz < MATCH_MIN){
-        score_general = score_general + countVert;
+        this.score_general = this.score_general + countVert;
+        this.addMatch2(elem_name, 1);
     }else if (countVert < MATCH_MIN){
-        score_general = score_general + countHoriz;
+        this.score_general = this.score_general + countHoriz;
+        this.addMatch2(elem_name, 2);
     }else{
-        score_general = score_general + (countHoriz + countVert)*3;
+        this.score_general = this.score_general + (countHoriz + countVert)*3;
+        this.addMatch2(elem_name, 3);
     }
     }
 };
