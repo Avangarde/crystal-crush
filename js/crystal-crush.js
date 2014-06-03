6b09a3f496ed4e7f93e7d565eda60771633dde46
Edward Rojas <edward.rojascl@gmail.com>
2014-06-03 23:26:28 +0200

    Link to test the game added

---
+++ js/crystal-crush.js
@@ -0,0 +1,68 @@
+// Example by https://twitter.com/awapblog
+// Modified by Avantgarde
+
+
+var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, 'phaser-example', {preload: preload, create: create, update: update, render: render});
+var gamePanel = null;
+var scorePanel = null;
+var alchemyPanel = null;
+
+var BUTTONWIDTH = 193;
+var BUTTONHEIGHT = 71;
+
+function preload() {
+    
+    game.load.image('background', 'assets/background.png');
+    
+    gamePanel = new GamePanel(game, xGamePanel, yGamePanel, widthGamePanel, heigthGamePanel);
+    scorePanel = new ScorePanel(game, xScorePanel, yScorePanel, widthScorePanel, heigthScorePanel);
+    alchemyPanel = new AlchemyPanel(game, xAlchemyPanel, yAlchemyPanel, widthAlchemyPanel, heightAlchemyPanel);
+
+    gamePanel.preload();
+    scorePanel.preload();
+    alchemyPanel.preload();
+}
+
+function create() {
+    game.world.setBounds(-canvasWidth + scorePanel, 0, 2*canvasWidth - scorePanel, canvasHeight);
+    var background = game.add.sprite(-canvasWidth + scorePanel.width, 0, 'background');
+    background.width = canvasWidth*2 - scorePanel.width;
+    background.height = canvasHeight;
+    
+    gamePanel.create();
+    scorePanel.create();
+    alchemyPanel.create();
+
+}
+
+function update() {
+    gamePanel.update();
+    scorePanel.update();
+    alchemyPanel.update();
+}
+
+function render() {
+//    game.debug.cameraInfo(game.camera, 32, 32);
+}
+
+function actionOnClick() {
+    animationScreen = true;
+}
+
+function animationCamera() {
+    if (!inAlchemyPanel) {
+        if (game.camera.x >= -canvasWidth + scorePanel.width + 2 * margin)
+            game.camera.x -= margin;
+        else {
+            animationScreen = false;
+            inAlchemyPanel = true;
+        }
+    } else {
+        if (game.camera.x < 0)
+            game.camera.x += margin;
+        else {
+            animationScreen = false;
+            inAlchemyPanel = false;
+        }
+    }
+}
---
+++ js/variables.js
@@ -0,0 +1,48 @@
+var canvasWidth = window.innerWidth * 10/16 < window.innerHeight ? window.innerWidth : window.innerHeight * 16/10;
+var canvasHeight = canvasWidth * 10 / 16 < window.innerHeight ? canvasWidth * 10 / 16 : window.innerHeight;
+var margin = canvasHeight * 0.015;
+
+var xScorePanel = margin;
+var yScorePanel = margin;
+var widthScorePanel = (canvasWidth * 0.3) - (2 * margin);
+var heigthScorePanel = canvasHeight - (2 * margin);
+
+var widthGamePanel = canvasHeight - (2 * margin);
+var heigthGamePanel = canvasHeight - (2 * margin);
+var xGamePanel = (canvasWidth + widthScorePanel + margin - widthGamePanel)/2;
+var yGamePanel = margin;
+
+var widthAlchemyPanel = canvasHeight - (2 * margin);
+var heightAlchemyPanel = canvasHeight - (2 * margin);
+var xAlchemyPanel = (2 * margin + widthAlchemyPanel) * -1;
+var yAlchemyPanel = margin;
+
+var BOARD_COLS = 6;
+var BOARD_ROWS = 6;
+var MATCH_MIN = 3;
+var ELEM_SIZE = (heigthGamePanel - (2 * margin)) / BOARD_COLS;
+
+var CU = "CU";
+var ZN = "ZN";
+var NA = "NA";
+var CL = "CL";
+var A = "A";
+var B = "B";
+var SELECT = "SELECT";
+var elemNames = [CU, ZN, NA, CL, A, B];
+
+var allowInput;
+var animationScreen=false;
+var inAlchemyPanel=false;
+var elements;
+var selectedElement;
+var selectedElementStartPos;
+var selectedElemTween;
+var tempShiftedElem = null;
+var matched = false;
+var selection;
+var stillGame;
+
+var tipoFuente = "20px Candela";
+var style1 = { font: "20px Arial", fill: "#ffffff", align: "center" };
+var style2 = { font: "20px Arial", fill: "#00ffff", align: "center" };
