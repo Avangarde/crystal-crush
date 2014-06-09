8c86d1b3702cca9181321fc4904dc4ae8a5f465b
Guido SOFER INGLESI <sofering@ensimag.fr>
2014-06-09 15:24:40 +0200

    a

--- crystal-crush/public_html/js/crystal-crush.js
+++ crystal-crush/public_html/js/crystal-crush.js
@@ -1,74 +1,23 @@
 // Example by https://twitter.com/awapblog
 // Modified by Avantgarde
 
-
-var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, 'phaser-example', {preload: preload, create: create, update: update, render: render});
-var gamePanel = null;
-var scorePanel = null;
-var alchemyPanel = null;
-
-var crystals;
-
-function preload() {
-
-    game.load.image('background', 'assets/background.png');
-    game.load.text('crystals', 'files/crystals.txt');
-    game.load.text('highScore', 'files/highScore');
-
-    gamePanel = new GamePanel(game, xGamePanel, yGamePanel, widthGamePanel, heigthGamePanel);
-    scorePanel = new ScorePanel(game, xScorePanel, yScorePanel, widthScorePanel, heigthScorePanel, 0);
-    alchemyPanel = new AlchemyPanel(game, xAlchemyPanel, yAlchemyPanel, widthAlchemyPanel, heightAlchemyPanel);
-
-    gamePanel.preload();
-    scorePanel.preload();
-    alchemyPanel.preload();
-
+var Main = {};
+function boot() {
+    var game = new Phaser.Game(480, 320);
+    game.state.add('preloader', CrystalCrush.Preloader, true);
+    game.state.add('boot', CrystalCrush.Boot, true);
+    game.state.add('home', CrystalCrush.Home, true);
+    game.state.add('lvl1', CrystalCrush.Lvl1);
+    game.state.add('lvl2', CrystalCrush.Lvl2);
+    game.state.add('lvl3', CrystalCrush.Lvl3);
+    game.state.add('highScore', CrystalCrush.HighScore);
 }
 
-function create() {
-    game.world.setBounds(-canvasWidth + scorePanel, 0, 2 * canvasWidth - scorePanel, canvasHeight);
-    var background = game.add.sprite(-canvasWidth + scorePanel.width, 0, 'background');
-    background.width = canvasWidth * 2 - scorePanel.width;
-    background.height = canvasHeight;
 
-    gamePanel.create();
-    alchemyPanel.create();
-    scorePanel.create();
-    scorePanel.setHighScore(parseInt(game.cache.getText('highScore')));
 
-    var t = game.cache.getText('crystals');
-    crystals = t.split('\n');
 
-}
 
-function update() {
-    gamePanel.update();
-    scorePanel.update();
-    alchemyPanel.update();
-}
+//var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, 'phaser-example', {preload: preload, create: create, update: update, render: render});
 
-function render() {
-//    game.debug.cameraInfo(game.camera, 32, 32);
-}
 
-function actionOnClick() {
-    animationScreen = true;
-}
 
-function animationCamera() {
-    if (!inAlchemyPanel) {
-        if (game.camera.x >= -canvasWidth + scorePanel.width + 2 * margin)
-            game.camera.x -= margin;
-        else {
-            animationScreen = false;
-            inAlchemyPanel = true;
-        }
-    } else {
-        if (game.camera.x < 0)
-            game.camera.x += margin;
-        else {
-            animationScreen = false;
-            inAlchemyPanel = false;
-        }
-    }
-}
