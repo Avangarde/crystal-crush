7d0a4d079738c4118a29a64185367759ea6b6636
Jmmartinezro <Jmmartinezro@hotmail.com>
2014-06-10 23:12:57 +0200

    Merge Bugs Patch #2

--- crystal-crush/public_html/js/Lvl1.js
+++ crystal-crush/public_html/js/Lvl1.js
@@ -1,6 +1,6 @@
-CrystalCrush.Lvl1 = function(game){
+CrystalCrush.Lvl1 = function(game) {
     this.game = game;
-    this.game.numMoves=21;
+    this.game.numMoves = 1;
 };
 
 var gamePanel = null;
@@ -11,35 +11,34 @@ var lostPanel = null;
 var crystals;
 
 CrystalCrush.Lvl1.prototype = {
+    preload: function() {
 
-preload: function () {
+        gamePanel = new GamePanel(game, xGamePanel, yGamePanel, widthGamePanel, heigthGamePanel);
+        scorePanel = new ScorePanel(game, xScorePanel, yScorePanel, widthScorePanel, heigthScorePanel, 0);
+        alchemyPanel = new AlchemyPanel(game, xAlchemyPanel, yAlchemyPanel, widthAlchemyPanel, heightAlchemyPanel);
+        lostPanel = new LostPanel(game);
 
-    gamePanel = new GamePanel(game, xGamePanel, yGamePanel, widthGamePanel, heigthGamePanel);
-    scorePanel = new ScorePanel(game, xScorePanel, yScorePanel, widthScorePanel, heigthScorePanel, 0);
-    alchemyPanel = new AlchemyPanel(game, xAlchemyPanel, yAlchemyPanel, widthAlchemyPanel, heightAlchemyPanel);
-    lostPanel = new LostPanel(game);
-    
-         },
-create : function () {
-    game.world.setBounds(-canvasWidth + scorePanel, 0, 2 * canvasWidth - scorePanel, canvasHeight);
-    this.background = game.add.sprite(-canvasWidth + scorePanel.width, 0, 'background');
-    this.background.width = canvasWidth * 2 - scorePanel.width;
-    this.background.height = canvasHeight;
+    },
+    create: function() {
+        game.world.setBounds(-canvasWidth + scorePanel, 0, 2 * canvasWidth - scorePanel, canvasHeight);
+        this.background = game.add.sprite(-canvasWidth + scorePanel.width, 0, 'background');
+        this.background.width = canvasWidth * 2 - scorePanel.width;
+        this.background.height = canvasHeight;
 
-    gamePanel.create();
-    alchemyPanel.create();
-    scorePanel.create();
-    scorePanel.highScore = localStorage.getItem("highScore") === null ? 
-    0 : localStorage.getItem("highScore");
+        gamePanel.create();
+        alchemyPanel.create();
+        scorePanel.create();
+        scorePanel.highScore = localStorage.getItem("highScore") === null ?
+                0 : localStorage.getItem("highScore");
 
-    var t = game.cache.getText('crystals');
-    crystals = t.split('\n');
+        var t = game.cache.getText('crystals');
+        crystals = t.split('\n');
 
-         },
-update : function () {
-    gamePanel.update();
-    scorePanel.update();
-    alchemyPanel.update();
-    lostPanel.update();
-}
+    },
+    update: function() {
+        gamePanel.update();
+        scorePanel.update();
+        alchemyPanel.update();
+        lostPanel.update();
+    }
 }
