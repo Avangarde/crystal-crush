6f45b19c28e76c1d9e336ee8bb46f3acb5823cce
Guido SOFER INGLESI <sofering@ensimag.fr>
2014-06-13 15:11:19 +0200

    guido

--- crystal-crush/public_html/js/Lvl_panels/PopUpPanel.js
+++ crystal-crush/public_html/js/Lvl_panels/PopUpPanel.js
@@ -21,24 +21,71 @@ PopUpPanel = function(game, x , y , width , height, father, type) {
 
 PopUpPanel.prototype = {
 create: function() {
-    this.background = game.add.sprite(this.x, this.y, 'PopUpBackground');
-    this.background.width = this.width;
-    this.background.height = this.height;
-    game.paused = true;
-    // ACA TENES QUE PONER PAUSA ANDY!!!
-    game.input.onDown.add(this.unpause, self);
-    
-    this.xButton = game.add.sprite(this.x + this.width - 55, this.y + 5, 'xButton');
-    this.xButton.width = 50;
-    this.xButton.height = 50;
+    if(popUpPanel === 'welcome'){
+        this.createWelcome();
+    } else if (popUpPanel === 'info'){
+        this.createInfo();
+    } else if (popUpPanel === 'tuto'){
+        this.createTuto();
+    }
 },
+createWelcome: function (){
+        this.background = game.add.sprite(this.x, this.y, 'PopUpBackground');
+        this.background.width = this.width;
+        this.background.height = this.height;
+        game.paused = true;
+        // ACA TENES QUE PONER PAUSA ANDY!!!
+        game.input.onDown.add(this.unpause, self);
+        
+        this.xButton = game.add.sprite(this.x + this.width - 55, this.y + 5, 'xButton');
+        this.xButton.width = 50;
+        this.xButton.height = 50;
+},
+createInfo: function (){
+        this.background = game.add.sprite(this.x, this.y, 'PopUpBackground');
+        this.background.width = this.width;
+        this.background.height = this.height;
+        game.paused = true;
+        // ACA TENES QUE PONER PAUSA ANDY!!!
+        game.input.onDown.add(this.unpause, self);
+        
+        this.xButton = game.add.sprite(this.x + this.width - 55, this.y + 5, 'xButton');
+        this.xButton.width = 50;
+        this.xButton.height = 50;
+},
+createTuto: function (){
+        this.background = game.add.sprite(this.x, this.y, 'PopUpBackground');
+        this.background.width = this.width;
+        this.background.height = this.height;
+        game.paused = true;
+        // ACA TENES QUE PONER PAUSA ANDY!!!
+        game.input.onDown.add(this.unpause, self);
+        
+        this.xButton = game.add.sprite(this.x + this.width - 55, this.y + 5, 'xButton');
+        this.xButton.width = 50;
+        this.xButton.height = 50;
+},
+
 update: function() {
-        },
+},
+
 unpause: function(event) {
     if (!game.paused) {
         return;
     }
 
+    console.log('here we are');
+    console.log(popUpPanel);
+    if(popUpPanel === 'welcome'){
+        console.log('enter the if');
+        this.unpauseWelcome(event);
+    } else if (popUpPanel === 'info'){
+        this.unpauseInfo(event);
+    } else if (popUpPanel === 'tuto'){
+        this.unpauseTuto(event);
+    }
+},
+unpauseWelcome: function(event){
     if (eventInBorder(event, popUpPanel.xButton)){
         game.paused = false;
         popUpPanel.destroypopUp();
@@ -55,6 +102,10 @@ unpause: function(event) {
             popUpPanel.destroypopUp();
         }
     }
+},
+unpauseInfo: function(event){
+},
+unpauseTuto: function(event){
 },
 
 
