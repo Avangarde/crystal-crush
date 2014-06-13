c8bb72cc7422f88f6664dc22374e1648a5ac9bb6
Guido SOFER INGLESI <sofering@ensimag.fr>
2014-06-13 11:38:44 +0200

    guido se manda cagadas, pero las arregla

---
+++ crystal-crush/public_html/js/Lvl_panels/WelcomePopUp.js
@@ -0,0 +1,26 @@
+
+WelcomePopUp = function(game) {
+    this.game = game;
+    this.popup;
+    this.playButton;
+};
+WelcomePopUp.prototype = {
+    create: function() {
+        this.popup = new PopUpPanel(game, xPopup, yPopup, widthPopup, heightPopup, this);
+        popUpPanel = this.popup
+        popUpName = 'welcome';
+        this.popup.create();
+
+        this.playButton = game.add.sprite(this.popup.x + this.popup.width / 4, this.popup.y + this.popup.height * 5 / 8, 'playButton');
+        this.playButton.width = this.popup.width / 2;
+        this.playButton.height = this.popup.height / 4;
+    },
+    update: function() {
+    },
+    destroy: function(){
+        this.playButton.kill();
+    },
+    eventInButton: function(event){
+        return eventInBorder(event, this.playButton);
+    }
+}
